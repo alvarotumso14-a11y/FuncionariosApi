29145d78840cde79045cd09031a19444c154a92a
Álvaro Santos Ribeiro <Alvarotumso14@gmail.com>
2026-08-19 19:40:44 -0300

    Adicionar arquivos de projeto.

---
+++ 04-Domain/Entities/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04_Domain.Entities
+{
+    public class Funcionario
+    {
+           public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salario { get; set; }
+        public string Cargo { get; set; }
+        public bool Ativo { get; set; } = true;
+    }
+}
